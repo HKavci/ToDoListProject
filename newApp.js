4e1dd30e7b317243c8947543229467080772ce2c
HakanKavci <hackankavci0@gmail.com>
2023-01-30 16:52:44 +0300

    .

--- newApp.js
+++ newApp.js
@@ -5,73 +5,72 @@ const ul = document.querySelector(".ul");
 const refresh = document.getElementById("refresh");
 const music = document.getElementById("background-music");
 
-music.autoplay = true
-music.volume = 0.2
-music.loop = true
+music.autoplay = true;
+music.volume = 0.2;
+music.loop = true;
 
 //Local storage dan veri alımı
 let todoList = JSON.parse(localStorage.getItem("todoList")) || [];
 
 window.addEventListener("load", () => {
-    getTodoListFromLocalStorage();
+  getTodoListFromLocalStorage();
 });
 
 const getTodoListFromLocalStorage = () => {
-    todoList.forEach((todo)=> {
-        createTodo(todo)
-    })
+  todoList.forEach((todo) => {
+    createTodo    (todo);
+  });
 };
 
 plus.addEventListener("click", (e) => {
-
-    if (input.value.trim().length !== 0) {
-          const newTodo = {
-          id: new Date().getTime(), //anlık milisaniye, uniq id için
-          status: false,
-          text: input.value,
-      };
-      createTodo(newTodo);
-      todoList.push(newTodo)
-      localStorage.setItem("todoList", JSON.stringify(todoList))
-}
+  if (input.value.trim().length !== 0) {
+    const newTodo = {
+      id: new Date().getTime(), //anlık milisaniye, uniq id için
+      status: false,
+      text: input.value,
+    };
+    createTodo(newTodo);
+    todoList.push(newTodo);
+    localStorage.setItem("todoList", JSON.stringify(todoList));
+  }
 });
 
 const createTodo = (newTodo) => {
-    const {id, status, text} = newTodo //Object destructuring
-    const li = document.createElement("li") //creating li element
-    li.setAttribute("id", id)
-    li.classList.add("li")
-    li.innerHTML = `<input type="checkbox" id="checkbox"></input> ${text} <i class="fa-solid fa-trash-can"></i>`
-    ul.appendChild(li)
-    input.value = ""
-}
+  const { id, status, text } = newTodo; //Object destructuring
+  const li = document.createElement("li"); //creating li element
+  li.setAttribute("id", id);
+  li.classList.add("li");
+  li.innerHTML = `<input type="checkbox" id="checkbox"></input> ${text} <i class="fa-solid fa-trash-can"></i>`;
+  ul.appendChild(li);
+  input.value = "";
+};
 
 ul.addEventListener("click", (e) => {
-    const idAttr = e.target.parentElement.getAttribute("id")
-    if (e.target.className == "fa-solid fa-trash-can") {
+  const idAttr = e.target.parentElement.getAttribute("id");
+  if (e.target.className == "fa-solid fa-trash-can") {
     e.target.parentElement.remove();
     e.stopPropagation;
-    todoList = todoList.filter((todo)=> todo.id != idAttr)
-    localStorage.setItem("todoList", JSON.stringify(todoList))
-} else if (e.target.id == "checkbox") {
+    todoList = todoList.filter((todo) => todo.id != idAttr);
+    localStorage.setItem("todoList", JSON.stringify(todoList));
+  } else if (e.target.id == "checkbox") {
     e.target.checked
-    ? (e.target.parentElement.style.textDecoration = "line-through")
-    : (e.target.parentElement.style.textDecoration = "");
-    todoList.map((todo)=>{
-        if (todo.id == idAttr) {todo.status = !todo.status}
-    })
-    localStorage.setItem("todoList", JSON.stringify(todoList)) //Burada sorun var. çözülecek !!!!!!!!!!!!!!!!!!!!!
-} 
+      ? (e.target.parentElement.style.textDecoration = "line-through")
+      : (e.target.parentElement.style.textDecoration = "");
+    todoList.map((todo) => {
+      if (todo.id == idAttr) {
+        todo.status = !todo.status;
+      }
+    });
+    localStorage.setItem("todoList", JSON.stringify(todoList)); //Burada sorun var. çözülecek !!!!!!!!!!!!!!!!!!!!!
+  }
 });
 
 refresh.addEventListener("click", () => {
-    window.location.reload();
+  window.location.reload();
 });
 
 input.addEventListener("keydown", (event) => {
-    if (event.code === "Enter" || event.code === "numpadEnter") {
-        plus.click()
-    }
+  if (event.code === "Enter" || event.code === "NumpadEnter") {
+    plus.click();
+  }
 });
-
-
